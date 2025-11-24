754b2c9f6295e1fdd566912bc1970a2cfc4e62fc
GwenP88 <12777@holbertonstudents.com>
2025-11-24 14:05:07 +0100

    add struct specifier in main.h

--- main.h
+++ main.h
@@ -10,16 +10,31 @@
     /* ---- main function ---- */
     int _printf(const char *format, ...);
 
-    /* ---- find function ---- */
-
-
-
     /* ---- print function ---- */
 
-    int print_char(va_list args);
+    int print_str(va_list args);
     int print_nb(va_list args);
 
     /* ---- utils function ---- */
 
+/* ---- Structure ---- */
+/**
+ * 
+ */
+
+typedef struct
+{
+    char *letter_spe;
+    void (*f)(va_list args);
+} specifier;
+
+specifier tab[] = 
+{
+    {'c', print_str},
+    {'s', print_str},
+    {'%', print_str},
+    {'d', print_nb},
+    {'i', print_nb},
+}
 
 #endif // MAIN_H
