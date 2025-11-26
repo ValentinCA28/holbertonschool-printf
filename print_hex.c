744d41e4cb89abe8be1a8c1dff9f8c84c86cbfc0
GwenP88 <12777@holbertonstudents.com>
2025-11-26 09:40:07 +0100

    merging the two print-hex functions into a single file

--- print_hex.c
+++ print_hex.c
@@ -12,3 +12,16 @@ int print_hex(va_list args)
 
 	return (print_unsigned_base(n, 16, "0123456789abcdef"));
 }
+
+/**
+ * print_HEX - handler pour %X (hexadécimal majuscule)
+ * @args: liste d'arguments variadiques
+ *
+ * Return: nombre de caractères imprimés
+ */
+int print_HEX(va_list args)
+{
+	unsigned int n = va_arg(args, unsigned int);
+
+	return (print_unsigned_base(n, 16, "0123456789ABCDEF"));
+}
